03656711921408d9293bc45f07325ed4b23e2afd
eastar93 <eastar93@naver.com>
2021-08-21 14:05:43 +0900

    SDK 210821 14:05

--- OurJspProject/src/kr/sc/ictjsp/test/model/testVO.java
+++ OurJspProject/src/kr/sc/ictjsp/test/model/testVO.java
@@ -2,6 +2,63 @@ package kr.sc.ictjsp.test.model;
 
 //문제들 풀엇을 때 db에 저장
 public class testVO {
-	private 
+	
+	private String htmlQ;
+	private String cssQ;
+	private String javaQ;
+	private String jspQ;
+	
+	public testVO() {}
+	
+	public testVO(String htmlQ, String cssQ, 
+				String javaQ, String jspQ) {
+		super();
+		this.htmlQ = htmlQ;
+		this.cssQ = cssQ;
+		this.javaQ = javaQ;
+		this.jspQ = jspQ;
+	}
+
+	public String getHtmlQ() {
+		return htmlQ;
+	}
+
+	public void setHtmlQ(String htmlQ) {
+		this.htmlQ = htmlQ;
+	}
+
+	public String getCssQ() {
+		return cssQ;
+	}
+
+	public void setCssQ(String cssQ) {
+		this.cssQ = cssQ;
+	}
+
+	public String getJavaQ() {
+		return javaQ;
+	}
+
+	public void setJavaQ(String javaQ) {
+		this.javaQ = javaQ;
+	}
+
+	public String getJspQ() {
+		return jspQ;
+	}
+
+	public void setJspQ(String jspQ) {
+		this.jspQ = jspQ;
+	}
+
+	@Override
+	public String toString() {
+		return "testVO [htmlQ=" + htmlQ + ", cssQ=" + cssQ + 
+				", javaQ=" + javaQ + ", jspQ=" + jspQ + "]";
+	}
+	
+	
+	
+	
 	
 }
