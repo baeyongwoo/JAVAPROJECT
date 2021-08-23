150921f113b97ee9dfe26aa6ed97c2eec3b846a3
BaeYW <86466971+baeyongwoo@users.noreply.github.com>
2021-08-23 17:41:39 +0900

    correctInsert

---
+++ OurJspProject/src/kr/sc/ictjsp/answer/service/InsertAnswerService.java
@@ -0,0 +1,14 @@
+package kr.sc.ictjsp.answer.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class InsertAnswerService implements AnswerService{
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+
+		
+	}
+
+}
--- OurJspProject/src/kr/sc/ictjsp/correct/service/CorrectInsert.java
+++ OurJspProject/src/kr/sc/ictjsp/correct/service/CorrectInsert.java
@@ -1,5 +1,40 @@
 package kr.sc.ictjsp.correct.service;
 
-public class CorrectInsert {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.sc.ictjsp.answer.model.AnswerDAO;
+import kr.sc.ictjsp.correct.model.CorrectDAO;
+import kr.sc.ictjsp.correct.model.CorrectVO;
+
+public class CorrectInsert implements CorrectService {
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			request.setCharacterEncoding("utf-8");
+			response.setCharacterEncoding("utf-8");
+			
+			String c_code = (String) request.getParameter("ccode");
+			String correct = (String) request.getParameter("correct");
+
+			int ccode = Integer.parseInt(c_code);
+
+			System.out.println("코드 정보 : " + ccode + "정답 : " + correct);
+			CorrectDAO dao = CorrectDAO.getInstance();
+			CorrectVO Input_correct = new CorrectVO(ccode, correct);
+
+			int result = dao.InsertCorrect(Input_correct);
+
+			if (result == 1) {
+				System.out.println("답안지 생성 완료");
+			} else if(result == 0){
+				System.out.println("답안지 생성 실패");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 }
