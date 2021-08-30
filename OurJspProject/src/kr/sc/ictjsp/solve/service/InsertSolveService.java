dc481cc661b23e837eebd4418c28b3684fb7bbc4
DESKTOP-C750Q06\dyddn <dyddn30612@naver.com>
2021-08-30 20:04:14 +0900

    insertsolveservice

--- OurJspProject/src/kr/sc/ictjsp/solve/service/InsertSolveService.java
+++ OurJspProject/src/kr/sc/ictjsp/solve/service/InsertSolveService.java
@@ -26,7 +26,7 @@ public class InsertSolveService implements SolveService{
 		
 		System.out.println("form에서 입력한 값 " + tcode + rsolve + suser);
 		
-		SolveDAO dao = SolveDAO.getInstance();
+		SolveDAO sdao = SolveDAO.getInstance();
 		SolveVO s_user = new SolveVO();
 		s_user.setTcode(tcode);
 		s_user.setSolve(rsolve);
@@ -35,18 +35,24 @@ public class InsertSolveService implements SolveService{
 		usersDAO udo = usersDAO.getInstance();
 		usersVO user = new usersVO();
 		
-		int result = dao.InsertSolve(s_user);
+		int result = sdao.InsertSolve(s_user);
 		
 		if(result == 1) {
 			System.out.println("답안 작성 완료");
 			request.setAttribute("solve_code", s_user.getTcode());
 			request.setAttribute("solve_solve", s_user.getSolve());
 			request.setAttribute("solve_user", s_user.getSuer());
-			request.setAttribute("userInsert", user.getUqcode());
 			System.out.println("푼사람 " + s_user.getSuer());
 			
+			//답안 작성 후 uid를 가지고 Uqcode를 userVO에 데이터 저장
+			user.setUid(suser);
+			System.out.println(user.getUid() + "의 저장된  정보");
+			udo.usergetinfo(user);
 			
+			user.setUqcode(recode);
+			sdao.userInsertCode(user);
 			
+			request.setAttribute("userInsert", user.getUqcode());
 			System.out.println("답안 제출 후 유저 정보 : " + user);
 		} else if(result ==0) {
 			System.out.println("답안 작성 실패");
